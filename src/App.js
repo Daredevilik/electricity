6e5c671cfec4cfdf8684dee5780143058827cde0
Dmitry Novozhilov <dmitry.novozilov@gmail.com>
2024-01-24 21:00:36 +0200

    Finish

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { useState } from 'react';
 import './App.scss';
+import Container from 'react-bootstrap/Container';
+import Head from './Head';
+import Body from './Body';
+import Footer from './Footer';
+import { DEFAULT_ACTIVE_BUTTON } from './Head/constants';
 
 function App() {
+  const [activePrice, setActivePrice] = useState(DEFAULT_ACTIVE_BUTTON);
+
   return (
-    <div></div>
+    <Container>
+      <Head activePrice={activePrice} setActivePrice={setActivePrice} />
+      <Body />
+      <Footer />
+    </Container>
   );
 }
 

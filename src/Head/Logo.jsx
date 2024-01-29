87fe104b561aaa37324721877f6466ec0842769d
Dmitry Novozhilov <dmitry.novozilov@gmail.com>
2024-01-29 09:32:58 +0200

    Added offcanvas

--- src/Head/Logo.jsx
+++ src/Head/Logo.jsx
@@ -1,8 +1,40 @@
+import { useState } from 'react';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import logo from './logo.png';
 
 function Logo() {
-    return(
-        <Col></Col>
+
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+        <Col><img src={logo} alt="" height="30px" width="30px" /> <Button variant="primary" onClick={handleShow}>
+            Search
+        </Button>
+
+            <Offcanvas show={show} onHide={handleClose}>
+                <Offcanvas.Header closeButton>
+                    <Offcanvas.Title>Search form</Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                    <InputGroup className="mb-3">
+                        <Form.Control
+                            placeholder="Search text"
+                            aria-label="Search text"
+                            aria-describedby="basic-addon2"
+                        />
+                        <Button variant="outline-secondary" id="button-addon2">
+                            Search
+                        </Button>
+                    </InputGroup>
+                </Offcanvas.Body>
+            </Offcanvas>
+        </Col >
     );
 }
 

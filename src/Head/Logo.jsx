a864042bd8a39edc9b04f06dc16ab5adc29a2f17
Dmitry Novozhilov <dmitry.novozilov@gmail.com>
2024-02-14 19:03:21 +0200

    HW9

--- src/Head/Logo.jsx
+++ src/Head/Logo.jsx
@@ -1,8 +1,15 @@
 import Col from 'react-bootstrap/Col';
 import logo from './logo.png';
 import Button from 'react-bootstrap/Button';
+import { setShowSideBar } from '../services/stateService';
+import { useDispatch } from 'react-redux';
 
-function Logo({ handleOpenSideBar }) {
+function Logo() {
+
+    const dispatch = useDispatch();
+    const handleOpenSideBar = () => {
+        dispatch(setShowSideBar(true));
+    };
 
     return (
         <>
